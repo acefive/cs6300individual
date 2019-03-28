ae582c3694505df8c005109ce0dfa82a9661e5ad
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-03-28 18:12:27 -0500

    added import junit

---
+++ Assignment7/test/edu/gatech/seclass/CoverageClassTestBC1.java
@@ -0,0 +1,11 @@
+package edu.gatech.seclass;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class CoverageClassTestBC1 {
+}
